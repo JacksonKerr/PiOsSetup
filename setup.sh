247cf130d6e8553ea578c32c159858ea5696a1bb
Jackson Kerr <jacksonkerr2@gmail.com>
2025-10-11 18:16:48 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 sudo apt update
 sudo apt --assume-yes full-upgrade
-sudo apt --assume-yes install sway firefox blueman
+sudo apt --assume-yes install sway firefox blueman pulseaudio pulseaudio-module-bluetooth
+systemctl --user enable pulseaudio.socket
+curl https://download.argon40.com/argon-eeprom.sh | bash
+curl https://download.argon40.com/argon1.sh | bash
 sudo reboot
