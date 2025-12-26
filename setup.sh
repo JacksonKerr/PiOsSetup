056132b9c745b3dc72dc72308ce5094bbe2a14d3
Jackson Kerr <jacksonkerr2@gmail.com>
2025-12-26 17:34:00 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,23 +5,16 @@
 sudo apt update
 sudo apt --assume-yes full-upgrade
 
-sudo apt --assume-yes install git sway firefox nautilus pulseaudio btop
+sudo apt --assume-yes install git i3 firefox nautilus pulseaudio btop
 
-wget https://download.argon40.com/scripts/argon-shutdown.sh
-sudo chmod 755 argon-shutdown.sh
-sudo mkdir /lib/systemd/system-shutdown/
-sudo mv argon-shutdown.sh /lib/systemd/system-shutdown/
 curl https://download.argon40.com/argon-eeprom.sh | bash
 curl https://download.argon40.com/argon1.sh | bash
 
 cp -r PiOsSetup/.config ~
 cp PiOsSetup/.profile ~/.profile
-#cp PiOsSetup/shutdown shutdown
-#sudo chmod +x shutdown
 
-#git clone https://github.com/Botspot/pi-apps && ~/pi-apps/install
-#pi-apps/manage install Signal
-#pi-apps/manage install VSCodium
+git clone https://github.com/Botspot/pi-apps && ~/pi-apps/install
+pi-apps/manage install VSCodium
 
 # TODO: None of this works!
 sudo apt --assume-yes install pulseaudio pavucontrol #pulseaudio-module-bluetooth blueman
